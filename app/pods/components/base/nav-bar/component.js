e83cdb3070292ddb6969636969b68e536468bf8b
Jatin Katyal <jatin.katyal13@gmail.com>
2020-10-13 16:37:57 +0530

    add: logout functionality

--- app/pods/components/base/nav-bar/component.js
+++ app/pods/components/base/nav-bar/component.js
@@ -4,13 +4,23 @@ import { action } from '@ember/object';
 import { later } from '@ember/runloop'
 
 export default class BaseNavBarComponent extends Component {
+  @service session;
   @service currentUser;
   @service sideBar;
+  @service router;
+
+  showPopup = false;
 
   @action
   toggleSidebar() {
     later(() => {
-      this.sideBar.toggleSidebar()
+      this.sideBar.toggleSidebar();
     }, 200)
+  }
+
+  @action
+  logout() {
+    this.session.invalidate();
+    this.router.transitionTo('login');
   }
 }
