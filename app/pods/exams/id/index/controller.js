b93cc1cebd4fe573e9518dfc376243de09f5bb97
Jatin Katyal <jatin.katyal13@gmail.com>
2020-12-10 18:53:18 +0530

    fix: show start button when the countdown timer ends

--- app/pods/exams/id/index/controller.js
+++ app/pods/exams/id/index/controller.js
@@ -1,9 +1,4 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
-export default class ExamsIdIndexController extends Controller {
-  @action
-  onEnd() {
-    console.log("ended");
-  }
-}
+export default class ExamsIdIndexController extends Controller {}
--- app/pods/exams/id/index/route.js
+++ app/pods/exams/id/index/route.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default class ExamsIdIndexRoute extends Route {
+  model() {
+    const { examination } = this.modelFor('exams.id');
+    return hash({
+      examination
+    })
+  }
+
+  setupController(controller, model) {
+    super.setupController(...arguments);
+    controller.set('actions', {
+      onEnd: () => this.refresh()
+    });
+  }
 }
