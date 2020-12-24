5aaa32321e72a6c3ccbc76301773c0cf00c9d7bd
Jatin Katyal <jatin.katyal13@gmail.com>
2020-12-25 01:03:20 +0530

    add: live proctoring via video recording

--- app/pods/components/exam-attempt/video-recorder/component.js
+++ app/pods/components/exam-attempt/video-recorder/component.js
@@ -1,7 +1,35 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { dropTask } from 'ember-concurrency-decorators';
+import { timeout } from 'ember-concurrency';
 
 export default class ExamAttemptVideoRecorderComponent extends Component {
+  @service api;
+
   stream = null;
+
+  @dropTask
+  *videoPingTask() {
+    while(true) {
+      try {
+        yield timeout(3000);
+        const track = this.stream.getTracks()[0];
+        const imageCapture = new ImageCapture(track);
+        const frame = yield imageCapture.takePhoto();
+
+        const response = yield this.api.request(`/exam-attempts/${this.examAttempt.id}/videoPing`, {
+          method: 'POST',
+          body: '{}'
+        })
+        yield fetch(response.url, {
+          method: 'PUT',
+          body: frame
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
 
   didInsertElement() {
     const constraints = {
@@ -14,11 +42,16 @@ export default class ExamAttemptVideoRecorderComponent extends Component {
     });
   }
 
+  didReceiveAttrs() {
+    this.videoPingTask.perform();
+  }
+
   willDestroyElement() {
     if (this.stream) {
       this.stream.getTracks().forEach(track => {
         track.stop()
       })
     }
+    this.videoPingTask.cancelAll();
   }
 }
