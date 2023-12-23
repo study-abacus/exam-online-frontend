f61bc8edff17e66266b7f39e1b4b693d2a4c6a3d
Prerna110797 <writeprernasingh@gmail.com>
2023-12-23 21:39:01 +0530

    changes in the login

--- app/pods/login/controller.js
+++ app/pods/login/controller.js
@@ -1,7 +1,47 @@
+// import Controller from "@ember/controller";
+// import { dropTask } from "ember-concurrency-decorators";
+// import { inject as service } from "@ember/service";
+// import { action, computed } from "@ember/object";
+// import { tracked } from "@glimmer/tracking";
+
+// export default class LoginController extends Controller {
+//   @service session;
+//   @service currentUser;
+//   @service api;
+
+//   @tracked state = "phone-input";
+
+//   phone = "";
+//   otp = "";
+
+//   @dropTask
+//   *sendOtp() {
+//     yield this.api.request(`/password/`, {
+//       method: "POST",
+//       body: JSON.stringify({
+//         phone: this.phone,
+//       }),
+//     });
+//     this.state = "otp-input";
+//   }
+
+//   @dropTask
+//   *login() {
+//     yield this.session.authenticate("authenticator:jwt", {
+//       phone: this.phone,
+//       otp: this.otp,
+//     });
+//     yield this.currentUser.load();
+//     this.transitionToRoute("dashboard");
+//   }
+// }
+
+// Updated controller.js
+// Updated controller.js
 import Controller from "@ember/controller";
 import { dropTask } from "ember-concurrency-decorators";
 import { inject as service } from "@ember/service";
-import { action, computed } from "@ember/object";
+import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
 
 export default class LoginController extends Controller {
@@ -9,29 +49,22 @@ export default class LoginController extends Controller {
   @service currentUser;
   @service api;
 
-  @tracked state = "phone-input";
+  @tracked state = "username-input";
 
-  phone = "";
-  otp = "";
-
-  @dropTask
-  *sendOtp() {
-    yield this.api.request(`/otp/`, {
-      method: "POST",
-      body: JSON.stringify({
-        phone: this.phone,
-      }),
-    });
-    this.state = "otp-input";
-  }
+  username = "";
+  password = "";
 
   @dropTask
   *login() {
-    yield this.session.authenticate("authenticator:jwt", {
-      phone: this.phone,
-      otp: this.otp,
-    });
-    yield this.currentUser.load();
-    this.transitionToRoute("dashboard");
+    try {
+      yield this.session.authenticate("authenticator:jwt", {
+        username: this.username,
+        password: this.password,
+      });
+      yield this.currentUser.load();
+      this.transitionToRoute("dashboard");
+    } catch (error) {
+      this.loginError = true;
+    }
   }
 }
